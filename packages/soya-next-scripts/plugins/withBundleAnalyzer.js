c354305dd10f01fed2120f5d37b615fef62af8f1
Khaer Ansori <khaer.ansori@traveloka.com>
2018-03-27 23:33:21 +0700

    [FIX] add bundle analyzer default config

---
+++ packages/soya-next-scripts/plugins/withBundleAnalyzer.js
@@ -0,0 +1,28 @@
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+
+module.exports = (nextConfig = {}) =>
+  Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { webpack } = withBundleAnalyzer({
+        analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: 'static',
+            reportFilename: '../../bundles/server.html'
+          },
+          browser: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/client.html'
+          }
+        }
+      });
+      const newConfig = webpack(config, options);
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(newConfig, options);
+      }
+
+      return newConfig;
+    }
+  });

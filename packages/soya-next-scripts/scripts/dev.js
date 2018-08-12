d4ea8eade056a96bd3b36db1e4478ab7d0c287ca
Khaer Ansori <khaer.ansori@traveloka.com>
2018-08-12 16:13:36 +0700

    [FEATURE] add frameguard on dev

--- packages/soya-next-scripts/scripts/dev.js
+++ packages/soya-next-scripts/scripts/dev.js
@@ -6,6 +6,7 @@ process.on("unhandledRejection", err => {
 });
 
 const express = require("express");
+const frameguard = require("frameguard");
 const next = require("next");
 const { join } = require("path");
 const { createRouter } = require("soya-next/server/router");
@@ -16,10 +17,10 @@ const conf = require("../next.config");
 
 const { appDir } = require("../config/paths");
 const app = next({
-  dev: true
+  dev: true,
   // @remove-on-eject-begin
   // eslint-disable-next-line comma-style
-  , conf
+  conf
   // @remove-on-eject-end
 });
 const argv = process.argv ? process.argv.slice(2) : [];
@@ -45,6 +46,7 @@ app
   )
   .then((soyaMiddleware = null) => {
     const server = express();
+    server.use(frameguard(config.server.frameguard));
     if (soyaMiddleware !== null) {
       server.use(soyaMiddleware);
     }

92244c3cdebe22ba494b14e148cbe033ef6ebd08
Sayuti Daniel <sayuti@traveloka.com>
2018-09-21 00:47:50 +0700

    Pre-compile document

--- packages/soya-next-scripts/plugins/withDocument.js
+++ packages/soya-next-scripts/plugins/withDocument.js
@@ -1,4 +1,4 @@
-const { join, normalize } = require("path");
+const { join } = require("path");
 
 module.exports = (nextConfig = {}) =>
   Object.assign({}, nextConfig, {
@@ -11,29 +11,18 @@ module.exports = (nextConfig = {}) =>
 
       if (options.isServer) {
         const entry = config.entry;
-        config.entry = () =>
-          entry().then(entries => {
-            const names = Object.keys(entries);
-            const name = names.find(
-              name => name === normalize("bundles/pages/_document.js")
-            );
-            const [documentPageEntry] = entries[name];
-            if (documentPageEntry !== "./pages/_document.js") {
-              entries[name] = [
-                `${require.resolve("../pages/_document")}?entry`
-              ];
-            }
-            return entries;
-          });
-
-        const pagesDir = join(__dirname, "..", "pages");
-        config.module.rules.push({
-          test: /\.jsx?$/,
-          include: pagesDir,
-          exclude: str =>
-            /node_modules/.test(str) && str.indexOf(pagesDir) !== 0,
-          use: options.defaultLoaders.babel
-        });
+        config.entry = async () => {
+          const entries = await entry();
+          const names = Object.keys(entries);
+          const name = names.find(
+            name => name === `static/${options.buildId}/pages/_document.js`
+          );
+          const [documentPageEntry] = entries[name];
+          if (documentPageEntry !== "./pages/_document.js") {
+            entries[name] = [require.resolve("../pages/_document")];
+          }
+          return entries;
+        };
       }
 
       if (typeof nextConfig.webpack === "function") {

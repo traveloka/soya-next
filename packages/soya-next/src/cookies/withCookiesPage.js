350f39a459b65d4f57ed99cfb3ce5b99024253e8
Sayuti Daniel <sayuti@traveloka.com>
2018-09-27 23:31:27 +0530

    Fix cookies

--- packages/soya-next/src/cookies/withCookiesPage.js
+++ packages/soya-next/src/cookies/withCookiesPage.js
@@ -1,3 +1,4 @@
+/* global __NEXT_DATA__ */
 import React from "react";
 import PropTypes from "prop-types";
 import hoistStatics from "hoist-non-react-statics";
@@ -19,9 +20,10 @@ export default Page => {
     };
 
     static async getInitialProps(ctx) {
-      const cookies = process.browser
-        ? new Cookies()
-        : (ctx.req && ctx.req.universalCookies) || new Cookies();
+      const cookies =
+        process.browser || __NEXT_DATA__.nextExport
+          ? new Cookies()
+          : ctx.req && ctx.req.universalCookies;
       const props =
         Page.getInitialProps &&
         (await Page.getInitialProps({ ...ctx, cookies }));

26928b3399686a9f614fa601c2edfa455053fd43
Sayuti Daniel <sayuti@traveloka.com>
2018-12-07 18:17:43 +0700

    bump soya-next

--- packages/soya-next-cli/src/index.js
+++ packages/soya-next-cli/src/index.js
@@ -39,11 +39,11 @@ const dependencies = [
   "react-dom@16.5.2",
   "react-redux@5.0.7",
   "redux@4.0.0",
-  "soya-next@0.7.0",
-  "soya-next-server@0.7.0"
+  "soya-next@0.7.4",
+  "soya-next-server@0.7.4"
 ];
 
-const devDependencies = ["soya-next-scripts@0.7.3"];
+const devDependencies = ["soya-next-scripts@0.7.4"];
 
 const install = (cmd, dependencies, { dev = false, exact = true } = {}) => {
   const args = [];

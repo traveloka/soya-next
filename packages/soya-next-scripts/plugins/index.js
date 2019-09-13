c5ba4a0bb3616f5a35004091137c4ceccab4f78b
Angga Maulana <angga.maulana@traveloka.com>
2019-09-13 17:13:33 +0700

    fix: bundleAnalyzer

--- packages/soya-next-scripts/plugins/index.js
+++ packages/soya-next-scripts/plugins/index.js
@@ -22,6 +22,7 @@ module.exports = (nextConfig = {}) => {
       assetPrefix = config.basePath && config.basePath.test;
     }
   }
+  
   return compose(
     withSourceMaps,
     withResolver,
--- packages/soya-next-scripts/plugins/withBundleAnalyzer.js
+++ packages/soya-next-scripts/plugins/withBundleAnalyzer.js
@@ -1,13 +1,3 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 
-module.exports = (nextConfig = {}) =>
-  Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      const { webpack } = withBundleAnalyzer({ enabled: Boolean(process.env.BUNDLE_ANALYZE) })(nextConfig);
-      const newConfig = webpack(config, options);
-      if (typeof nextConfig.webpack === "function") {
-        return nextConfig.webpack(newConfig, options);
-      }
-      return newConfig;
-    }
-  });
+module.exports = withBundleAnalyzer({ enabled: Boolean(process.env.BUNDLE_ANALYZE) });

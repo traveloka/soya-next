56aba8772ee6d8fc7d8131874bb0e68fafcb80ad
Thomas <thomas.suryajaya@traveloka.com>
2021-04-16 14:20:25 +0700

    fix: change obsolete react-redux context (#430)
    * [FIX] Change obsolete react context
    
    * Downgrade to react 16 for unit test
    
    * Bump redux
    
    * Remove useEffect
    
    * Downgrade react version for consistency

--- packages/soya-next/src/redux/applyReducers.js
+++ packages/soya-next/src/redux/applyReducers.js
@@ -1,51 +1,45 @@
-import PropTypes from "prop-types";
 import React from "react";
 import hoistStatics from "hoist-non-react-statics";
 import getDisplayName from "../utils/getDisplayName";
 import { NEXT_STATICS } from "../constants/Statics";
+import { ReactReduxContext, Provider } from "react-redux";
 
 export default reducers => Component => {
   if (typeof reducers === "undefined") {
     return Component;
   }
 
-  class ApplyReducers extends React.Component {
-    static displayName = getDisplayName("applyReducers", Component);
+  function ApplyReducers(props) {
+    const flag = React.useRef(true);
+    const context = React.useContext(ReactReduxContext);
+    const store = props.store || context.store;
 
-    static async getInitialProps(ctx) {
-      if (!ctx.store.soya) {
+    if (flag.current) {
+      if (!store.soya) {
         throw new Error(
           "applyReducers must be used with Soya's redux enhancer"
         );
       }
-      ctx.store.addReducer(reducers);
-      return (
-        Component.getInitialProps && (await Component.getInitialProps(ctx))
+      store.addReducer(reducers);
+      flag.current = false;
+    }
+
+    return (
+      <Component {...props} />
+    );
+  }
+  ApplyReducers.displayName = getDisplayName("applyReducers", Component);
+  ApplyReducers.getInitialProps = async (ctx) => {
+    if (!ctx.store.soya) {
+      throw new Error(
+        "applyReducers must be used with Soya's redux enhancer"
       );
     }
-
-    static contextTypes = {
-      store: PropTypes.shape({
-        subscribe: PropTypes.func.isRequired,
-        dispatch: PropTypes.func.isRequired,
-        getState: PropTypes.func.isRequired
-      }).isRequired
-    };
-
-    constructor(props, context) {
-      super(props, context);
-      this.store = props.store || context.store;
-      if (!this.store.soya) {
-        throw new Error(
-          "applyReducers must be used with Soya's redux enhancer"
-        );
-      }
-      this.store.addReducer(reducers);
-    }
-
-    render() {
-      return <Component {...this.props} />;
-    }
+    ctx.store.addReducer(reducers);
+    return (
+      Component.getInitialProps && (await Component.getInitialProps(ctx))
+    );
   }
+
   return hoistStatics(ApplyReducers, Component, NEXT_STATICS);
 };

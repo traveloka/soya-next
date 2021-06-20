6fb51f33548dd68e592f66b4f56a55df79c64f8e
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2021-06-20 14:38:06 +0700

    feat(types-global): extends expess request interface

--- packages/soya-next/global.d.ts
+++ packages/soya-next/global.d.ts
@@ -5,3 +5,18 @@ declare interface URIError extends Error {
 }
 
 declare var __NEXT_DATA__: import("next/dist/next-server/lib/utils").NEXT_DATA;
+
+//
+// ### Caution
+//
+// Please **do not use** this type. This is only for
+// global augmentation purpose only.
+//
+declare type __$SoyaNextLocaleContext = import("./src/types").SoyaNextLocaleContext;
+declare type __$SoyaNextConfig = import("./src/types").SoyaNextConfig;
+
+declare namespace Express {
+  export interface Request extends __$SoyaNextLocaleContext {
+    redirects?: __$SoyaNextConfig["redirects"];
+  }
+}

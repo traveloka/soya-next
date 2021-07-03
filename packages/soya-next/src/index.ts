e07ed510af292b8d646176477d2a1be32b081145
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2021-07-03 23:57:21 +0700

    feat(soya_next-entrypoint): migrate to typescript

---
+++ packages/soya-next/src/index.ts
@@ -0,0 +1,9 @@
+export { default as createComponent } from "./components/createComponent";
+export { default as createPage } from "./pages/createPage";
+
+// Types Export
+export * from "./types";
+export type {
+  CreateComponentInjectedProps,
+  CreateComponentProps,
+} from "./components/createComponent";
